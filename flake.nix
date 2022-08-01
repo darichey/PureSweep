a532b59ddc8ac29dc05dbf111b3a399b876c37bd
David Richey <darichey1@gmail.com>
2022-08-01 00:20:05 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    easy-purescript-nix = {
+      url = "github:justinwoo/easy-purescript-nix";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, easy-purescript-nix, ... }@inputs:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      easy-ps = import easy-purescript-nix { inherit pkgs; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell
+        {
+          buildInputs = [
+            easy-ps.purs-0_15_2
+            easy-ps.spago
+            pkgs.nodejs-14_x
+          ];
+        };
+    };
+}
