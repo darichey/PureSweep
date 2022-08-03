1836890972bc59810340f5eb4a2baf3e24b90d17
David Richey <darichey1@gmail.com>
2022-08-02 19:58:27 -0500

    Track clicked cells

--- src/Main.purs
+++ src/Main.purs
@@ -1,19 +1,15 @@
--- module Main where
--- import Prelude
--- import Effect (Effect)
--- import Effect.Console (log)
--- import Model (debugField, makeRandomField)
--- main :: Effect Unit
--- main = do
---   field <- makeRandomField 10 10 10
---   log $ debugField field
 module Main where
 
 import Prelude
+
+import Data.Array (mapWithIndex, snoc)
+import Data.String (joinWith)
+import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.Hooks as Hooks
 import Halogen.VDom.Driver (runUI)
@@ -34,11 +30,24 @@ twclass = HP.prop (PropName "className")
 
 makeFieldComponent :: forall query input output m. Field -> H.Component query input output m
 makeFieldComponent (Field field) =
-  Hooks.component \_ _ ->
+  Hooks.component \_ _ -> Hooks.do
+    clicked /\ clickedId <- Hooks.useState []
     Hooks.pure do
-      HH.div [ twclass "grid grid-cols-10" ]
-        $ map
-            ( \(Cell cell) ->
-                HH.button [ twclass "text-center" ] [ HH.text $ show cell.underlying ]
-            )
-            field.cells
+      HH.div_
+        [ HH.div
+            [ twclass "grid grid-cols-10" ]
+            $ mapWithIndex
+                ( \i (Cell cell) ->
+                    HH.button
+                      [ twclass "text-center"
+                      , HE.onClick \_ -> Hooks.modify_ clickedId (addClicked i)
+                      ]
+                      [ HH.text $ show cell.underlying ]
+                )
+                field.cells
+        , HH.div_
+          [ HH.text $ joinWith ", " $ map show clicked ]
+        ]
+
+addClicked :: Int -> Array Int -> Array Int
+addClicked i arr = snoc arr i
