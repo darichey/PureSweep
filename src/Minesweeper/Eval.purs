d55c62f05231bdfd5318a1e9d5ff096d0290f94f
David Richey <darichey1@gmail.com>
2022-08-05 18:39:09 -0500

    Prefetch game images
    This fixes an issue where when revealing cells for the first time,
    the field isn't re-rendered all at once. For example, before this
    change, on the first re-render, only the 1s and 2s would be revealed,
    and then on the second re-render all of the 0s are revealed. It's very
    subtle, but definitely noticeable if the requests take a bit too long.
    This way, all of the cell images are preloaded before the first click,
    and the new state renders all at once.

--- src/Minesweeper/Eval.purs
+++ src/Minesweeper/Eval.purs
@@ -5,13 +5,8 @@ import Prelude
 import Control.Monad.Except (ExceptT, throwError)
 import Control.Monad.Except.Trans (lift)
 import Control.Monad.ST (ST)
-import Control.Monad.ST.Class (liftST)
-import Control.Monad.ST.Global (Global)
 import Data.Array.ST as STArray
 import Data.Maybe (fromJust)
-import Effect (Effect)
-import Effect.Class (liftEffect)
-import Effect.Console as Console
 import Minesweeper.Model (STField)
 import Minesweeper.Monad (MinesweeperF(..))
 import Partial.Unsafe (unsafePartial)
@@ -23,21 +18,6 @@ runMinesweeperF field = case _ of
   ModifyCell i f g -> do
     _ <- lift $ STArray.modify i f field.cells
     new <- lift $ STArray.peek i field.cells
-    pure $ g $ unsafePartial $ fromJust new
-  Explode -> throwError Explosion
-  GetDims f -> pure $ f field.dims
-
-runMinesweeperFWithLogging :: STField Global -> MinesweeperF ~> ExceptT Explosion Effect
-runMinesweeperFWithLogging field = case _ of
-  ModifyCell i f g -> do
-    old <- lift $ liftST $ STArray.peek i field.cells
-    _ <- lift $ liftST $ STArray.modify i f field.cells
-    new <- lift $ liftST $ STArray.peek i field.cells
-    _ <- lift $ liftEffect $ Console.log $
-      if old == new then
-        "Get " <> show i
-      else
-        "Modify " <> show i <> " from " <> show old <> " to " <> show new
     pure $ g $ unsafePartial $ fromJust new
   Explode -> throwError Explosion
   GetDims f -> pure $ f field.dims
